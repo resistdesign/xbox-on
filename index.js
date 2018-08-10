2354645ba8d9422d8f584f4698f81f4570b8cf8f
Ryan Graff <ryantgraff@resistdesign.com>
2018-08-09 21:59:59 -0400

    fix: Upgrade to use xbox-smartglass-core-node.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var dgram = require('dgram');
+const Smartglass = require('xbox-smartglass-core-node');
 
-const PORT = 5050;
 const DEFAULT_TRIES = 5;
-const DEFAULT_DELAY = 1000;
 
 module.exports = Xbox;
 
@@ -12,48 +10,20 @@ function Xbox(ip, id) {
   return this;
 }
 
-Xbox.prototype.powerOn = function(options, callback) {
-  options = options || {};
-  callback = callback || function() {};
+Xbox.prototype.powerOn = function (options, callback) {
+  Smartglass.power_on({
+    live_id: this.id, // Put your console's live id here (Required)
+    tries: DEFAULT_TRIES, // Number of packets too issue the boot command (Optional)
+    ip: this.ip // Your consoles ip address (Optional)
+  }, function (result) {
+    if (result) {
+      console.log('Device booted successfully', result);
+    } else {
+      console.log('Failed to boot device');
+    }
 
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-
-  // Retry up to options.tries times
-  for (var i = 0; i < (options.tries || DEFAULT_TRIES); i++) {
-    setTimeout((function(xbox, i) {
-      return function() {
-        xbox.sendOn();
-
-        // If wait was specified, callback after last try, otherwise, callback after first try
-        if (options.waitForCallback && i + 1 === options.tries) {
-          callback();
-        } else if (!options.waitForCallback && i === 0) {
-          callback();
-        }
-      }
-    })(this, i), i * (options.delay || DEFAULT_DELAY));
-  }
-};
-
-Xbox.prototype.sendOn = function(callback) {
-  callback = callback || function() {};
-
-  // Open socket
-  var socket = dgram.createSocket('udp4');
-
-  // Create payload
-  var powerPayload = new Buffer('\x00' + String.fromCharCode(this.id.length) + this.id + '\x00'),
-      powerPayloadLength = new Buffer(String.fromCharCode(powerPayload.length)),
-      powerHeader = Buffer.concat([new Buffer('dd0200', 'hex'), powerPayloadLength, new Buffer('0000', 'hex')]),
-      powerPacket = Buffer.concat([powerHeader, powerPayload]);
-
-  // Send
-  socket.send(powerPacket, 0, powerPacket.length, PORT, this.ip, function(err) {
-    if (err) return callback(err);
-    socket.close();
-    callback();
+    if (callback instanceof Function) {
+      callback();
+    }
   });
 };
